0ca6bdbcb34f9d0b8d9fd9033d444385bc753b40
Nano <nano.anderson@gmail.com>
2022-10-24 10:51:20 -1000

    Stub out portfolio and search views and add tab bar

--- MojoTest/ContentView.swift
+++ MojoTest/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            PortfolioView()
+                .badge(2)
+                .tabItem {
+                    Label("Portfolio", systemImage: "chart.pie.fill")
+                }
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
         }
-        .padding()
     }
 }
 
