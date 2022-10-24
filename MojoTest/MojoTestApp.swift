687af2374b443a9846df818b51139740946a2206
Nano <nano.anderson@gmail.com>
2022-10-24 10:01:30 -1000

    Initial Commit

---
+++ MojoTest/MojoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojoTestApp.swift
+//  MojoTest
+//
+//  Created by Nano Anderson on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MojoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
